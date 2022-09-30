7f95ab5dbfc1cffea4faced6a2d9dc2342dc8e29
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-09-30 17:56:40 +0900

    Create PoisonMatango.java

---
+++ 10_継承/10-3/PoisonMatango.java
@@ -0,0 +1,16 @@
+public class PoisonMatango extends Matango {
+	int poisonCount = 5;
+	public PoisonMatango(char suffix) {
+	    super(suffix);
+	}
+    public void attack(Hero h) {
+        super.attack(h);
+        if(this.poisonCount != 0) {
+            System.out.println("さらに毒の胞子をばらまいた！");
+            int damage = h.hp / 5;
+            h.hp -= damage;
+            System.out.println(damage + "ポイントのダメージ！");
+            this.poisonCount--;
+        }
+    }
+}
