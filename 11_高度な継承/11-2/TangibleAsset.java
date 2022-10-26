49454623ea59f365d72ccd144f6b66c87fc3c9d9
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-10-27 00:40:57 +0900

    Update TangibleAsset.java

--- 11_高度な継承/11-2/TangibleAsset.java
+++ 11_高度な継承/11-2/TangibleAsset.java
@@ -4,7 +4,7 @@ public abstract class TangibleAsset extends Asset {
     public TangibleAsset(String name, int price, String color) {
         super(name, price);
         this.color = color;
-	}
+    }
 	
     public String getColor() { return this.color; }
 }
