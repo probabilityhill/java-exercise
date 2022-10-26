61884096a8b8f1bed671139c676b8ea4913a45d2
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-10-27 00:40:11 +0900

    Update Asset.java

--- 11_高度な継承/11-2/Asset.java
+++ 11_高度な継承/11-2/Asset.java
@@ -1,12 +1,12 @@
 public abstract class Asset {
-	String name;
-	int price;
+    String name;
+    int price;
 	
-	public Asset(String name, int price) {
-	    this.name = name;
-	    this.price = price;
+    public Asset(String name, int price) {
+        this.name = name;
+        this.price = price;
 	}
 	
-	public String getName() { return this.name; }
-	public int getPrice() { return this.price; }
+    public String getName() { return this.name; }
+    public int getPrice() { return this.price; }
 }
