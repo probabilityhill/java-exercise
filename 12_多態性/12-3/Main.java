8396a9c036221d1a93ea819200d98e21cdb53049
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-10-29 12:31:31 +0900

    Create Main.java

---
+++ 12_多態性/12-3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Y[] ary = new ary[2];
+    	ary[0] = new A();
+    	ary[1] = new B();
+    	for(Y item : ary) {
+    	    item.a();
+    	}
+    }
+}
