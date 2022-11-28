6f6a2829f4489b979222214437fd630807a3a0f2
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-11-28 21:55:20 +0900

    Create Wand.java

---
+++ 13_カプセル化/13-2/Wand.java
@@ -0,0 +1,9 @@
+public class Wand {
+    private String name;     // 杖の名前
+    private double power;    // 杖の魔力
+    
+    public void getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+    public void getPower() { return this.power; }    
+    public void setPower(double power) { this.power = power; }
+}
