82335b87a38cc01aca3c43af3923deafb199b63f
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-12-03 01:23:58 +0900

    Create Main.java

---
+++ 15_文字列と日付の扱い/15-1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) { 
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 1; i <= 100; i++) {
+	        sb.append(i).append(",");
+	    }
+	    String s = sb.toString();
+	    String[] a = s.split(",");
+	}
+}
